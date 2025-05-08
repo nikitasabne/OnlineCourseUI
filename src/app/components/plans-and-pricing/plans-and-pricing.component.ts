0d9b8975141931ad8c19725a3086ce04fe454c4d
Nikita <nikitej01@gmail.com>
2025-05-08 13:11:32 +0530

    home page setup

---
+++ src/app/components/plans-and-pricing/plans-and-pricing.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plans-and-pricing',
+  imports: [],
+  templateUrl: './plans-and-pricing.component.html',
+  styleUrl: './plans-and-pricing.component.css'
+})
+export class PlansAndPricingComponent {
+
+}
